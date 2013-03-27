e150890fa27d918174af9e0cf5212df317449cf6
Richard Long <rlong.github@hexb.com>
2013-03-27 18:20:07 +1000

    jsonbroker.integration_test initial commit

---
+++ jsonbroker.integration_test/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  jsonbroker.integration_test
+//
+//  Created by Richard Long on 23/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
