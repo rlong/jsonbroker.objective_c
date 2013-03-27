0537ee86210433d5d9cbcaed5e1fde2afe244721
Richard Long <rlong.github@hexb.com>
2013-03-27 18:22:20 +1000

    jsonbroker.library.osx initial commit

---
+++ jsonbroker.library.osx/jsonbroker_library_osx.h
@@ -0,0 +1,13 @@
+//
+//  jsonbroker_library_osx.h
+//  jsonbroker.library.osx
+//
+//  Created by Richard Long on 25/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jsonbroker_library_osx : NSObject
+
+@end
---
+++ jsonbroker.library.osx/jsonbroker_library_osx.m
@@ -0,0 +1,23 @@
+//
+//  jsonbroker_library_osx.m
+//  jsonbroker.library.osx
+//
+//  Created by Richard Long on 25/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "jsonbroker_library_osx.h"
+
+@implementation jsonbroker_library_osx
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
