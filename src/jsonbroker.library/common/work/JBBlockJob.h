c85b80b6649ed9ca1ef011f20c5c453698cbd380
Richard Long <rlong.github@hexb.com>
2013-05-21 13:21:34 +1000

    pushed block logic into the class JBBlockJob

--- src/jsonbroker.library/common/work/JBBlockJob.h
+++ src/jsonbroker.library/common/work/JBBlockJob.h
@@ -5,26 +5,53 @@
 
 #import <Foundation/Foundation.h>
 
+#import "JBBaseException.h"
 #import "JBJob.h"
 
 
-typedef void(^BlockJobDelegate)(void);
+typedef id(^JBBlock)(id context);
+typedef void(^JBBlockDone)(id context, id blockResponse);
+typedef void(^JBBlockFailed)(id context, JBBaseException* exceptionThrown);
 
 
 @interface JBBlockJob : NSObject <JBJob> {
     
-    
+    // context
+    id _context;
+    //@property (nonatomic, retain) id context;
+    //@synthesize context = _context;
+
     // block
-    BlockJobDelegate _block;
-    //@property (nonatomic, copy) BlockJobDelegate block;
+    JBBlock _block;
+    //@property (nonatomic, copy) JBBlock block;
     //@synthesize block = _block;
     
+    // blockResponse
+    id _blockResponse;
+    //@property (nonatomic, retain) id blockResponse;
+    //@synthesize blockResponse = _blockResponse;
+
+    
+    // blockDone
+    JBBlockDone _blockDone;
+    //@property (nonatomic, copy) JBBlockDone blockDone;
+    //@synthesize blockDone = _blockDone;
+
+    // blockFailed
+    JBBlockFailed _blockFailed;
+    //@property (nonatomic, copy) JBBlockFailed blockFailed;
+    //@synthesize blockFailed = _blockFailed;
+
+    
 }
+
++(void)executeWithContext:(id)context block:(JBBlock)block;
++(void)executeWithContext:(id)context block:(JBBlock)block onBlockDone:(JBBlockDone)blockDone onBlockFailed:(JBBlockFailed) blockFailed;
 
 #pragma mark -
 #pragma mark instance lifecycle
 
--(id)initWithBlock:(BlockJobDelegate)block;
-
+-(id)initWithContext:(id)context block:(JBBlock)block;
+-(id)initWithContext:(id)context block:(JBBlock)block onBlockDone:(JBBlockDone)blockDone onBlockFailed:(JBBlockFailed) blockFailed;
 
 @end
--- src/jsonbroker.library/common/work/JBBlockJob.m
+++ src/jsonbroker.library/common/work/JBBlockJob.m
@@ -3,8 +3,11 @@
 //  Released under the MIT license ( http://opensource.org/licenses/MIT )
 //
 
-#import "JBBlockJob.h"
 
+#import "JBBaseException.h"
+#import "JBBlockJob.h"
+#import "JBLog.h"
+#import "JBWorkManager.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -12,10 +15,32 @@
 
 @interface JBBlockJob ()
 
+// context
+//id _context;
+@property (nonatomic, retain) id context;
+//@synthesize context = _context;
+
 // block
-//BlockJobDelegate _block;
-@property (nonatomic, copy) BlockJobDelegate block;
+//JBBlock _block;
+@property (nonatomic, copy) JBBlock block;
 //@synthesize block = _block;
+
+// blockResponse
+//id _blockResponse;
+@property (nonatomic, retain) id blockResponse;
+//@synthesize blockResponse = _blockResponse;
+
+// blockDone
+//JBBlockDone _blockDone;
+@property (nonatomic, copy) JBBlockDone blockDone;
+//@synthesize blockDone = _blockDone;
+
+// blockFailed
+//JBBlockFailed _blockFailed;
+@property (nonatomic, copy) JBBlockFailed blockFailed;
+//@synthesize blockFailed = _blockFailed;
+
+
 
 @end
 
@@ -33,50 +58,174 @@
 
 -(void)execute {
     
+    if( nil == _block ) {
+        Log_warn(@"nil == _block");
+        return;
+    }
     
+    
+    JBBaseException *exceptionCaught = nil;
+    
+    @try {
+        
+        id blockResponse = _block(_context);
+        [self setBlockResponse:blockResponse];
+    }
+    @catch (JBBaseException *exception) {
+        
+        exceptionCaught = exception;
+        
+        if( nil != _blockFailed ) {
+            
+            // vvv http://stackoverflow.com/questions/7364169/how-to-dispatch-a-block-with-parameter-on-main-queue-or-thread
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                @try {
+                    _blockFailed( _context, exception );
+                }
+                @catch (NSException *exception) {
+                    Log_errorException( exception );
+                }
+                
+            });
+            
+            // ^^^ http://stackoverflow.com/questions/7364169/how-to-dispatch-a-block-with-parameter-on-main-queue-or-thread
+            
+        } else {
+            Log_errorException( exception );
+        }
+        
+        // we are not 'done' so we return ...
+        return;
+        
+    }
+    
+    if( nil == exceptionCaught ) { // if we completed the call to '_asyncAdaptee' successfully ...
+        if( nil != _blockDone ) {
+            
+            // vvv http://stackoverflow.com/questions/7364169/how-to-dispatch-a-block-with-parameter-on-main-queue-or-thread
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                @try {
+                    _blockDone( _context, _blockResponse );
+                }
+                @catch (NSException *exception) {
+                    Log_errorException( exception );
+                }
+            });
+            // ^^^ http://stackoverflow.com/questions/7364169/how-to-dispatch-a-block-with-parameter-on-main-queue-or-thread
+            
+        }
+    }
 
-    _block();
-    
-    
 }
 
 
 
++(void)executeWithContext:(id)context block:(JBBlock)block {
+    
+    JBBlockJob* job = [[JBBlockJob alloc] initWithContext:context block:block];
+    {
+        
+        [JBWorkManager enqueue:job];
+        
+    }
+
+    [job release];
+
+}
+
++(void)executeWithContext:(id)context block:(JBBlock)block onBlockDone:(JBBlockDone)blockDone onBlockFailed:(JBBlockFailed) blockFailed {
+
+    JBBlockJob* job = [[JBBlockJob alloc] initWithContext:context block:(JBBlock)block onBlockDone:(JBBlockDone)blockDone onBlockFailed:(JBBlockFailed) blockFailed];
+    {
+        
+        [JBWorkManager enqueue:job];
+        
+    }
+    
+    [job release];
+
+}
+
 #pragma mark -
 #pragma mark instance lifecycle
 
--(id)initWithBlock:(BlockJobDelegate)block {
+-(id)initWithContext:(id)context block:(JBBlock)block {
     
     JBBlockJob* answer = [super init];
     
     if( nil != answer ) {
-        
+        [answer setContext:context];
         [answer setBlock:block];
-        
+        [answer setBlockDone:nil];
+        [answer setBlockFailed:nil];
     }
-    
     
     return answer;
     
+}
+
+-(id)initWithContext:(id)context block:(JBBlock)block onBlockDone:(JBBlockDone)blockDone onBlockFailed:(JBBlockFailed) blockFailed {
+    
+    JBBlockJob* answer = [super init];
+    
+    if( nil != answer ) {
+        [answer setContext:context];
+        [answer setBlock:block];
+        [answer setBlockDone:blockDone];
+        [answer setBlockFailed:blockFailed];
+    }
+    
+    return answer;
     
 }
 
 -(void)dealloc {
 	
-	[self setBlock:nil];
-	
+	[self setContext:nil];
+    [self setBlock:nil];
+	[self setBlockResponse:nil];
+	[self setBlockDone:nil];
+	[self setBlockFailed:nil];
+
 	[super dealloc];
 	
 }
 
 
+
 #pragma mark -
 #pragma mark fields
 
+// context
+//id _context;
+//@property (nonatomic, retain) id context;
+@synthesize context = _context;
+
+
 // block
-//BlockJobDelegate _block;
-//@property (nonatomic, copy) BlockJobDelegate block;
+//JBBlock _block;
+//@property (nonatomic, copy) JBBlock block;
 @synthesize block = _block;
+
+
+// blockResponse
+//id _blockResponse;
+//@property (nonatomic, retain) id blockResponse;
+@synthesize blockResponse = _blockResponse;
+
+// blockDone
+//JBBlockDone _blockDone;
+//@property (nonatomic, copy) JBBlockDone blockDone;
+@synthesize blockDone = _blockDone;
+
+// blockFailed
+//JBBlockFailed _blockFailed;
+//@property (nonatomic, copy) JBBlockFailed blockFailed;
+@synthesize blockFailed = _blockFailed;
 
 
 @end
